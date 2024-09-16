e1c864ad749e867066517a733989eadeb676f606
BlackIceNodeRunner <166552626+BlackIceNodeRunner@users.noreply.github.com>
2024-09-16 18:26:54 +0300

    Update base.sh

--- base.sh
+++ base.sh
@@ -40,11 +40,20 @@ echo -e "|           \033[0m🐧 \033[1;34mTelegram: \033[2;32mhttps://t.me/los_
 echo -e "└──────────────────────────────────────────────────────────────┘\033[0m";
 }
 
+update(){
+sudo apt update && sudo apt upgrade -y
+}
+
+install_soft(){
+sudo apt install tmux curl
+}
+
 rm_docker_old(){
 for pkg in docker.io docker-doc docker-compose docker-compose-v2 podman-docker containerd runc; do sudo apt-get remove $pkg; done
 }
 
-install_docker(){sudo apt-get update
+install_docker(){
+sudo apt-get update
 sudo apt-get install ca-certificates curl
 sudo install -m 0755 -d /etc/apt/keyrings
 sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
@@ -55,12 +64,4 @@ echo \
   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt-get update
 sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
-}
-
-update(){
-sudo apt update && sudo apt upgrade -y
-}
-
-install_soft(){
-sudo apt install tmux curl
 }
